69ca371d074191b6ec364c2cd7a695efc207d018
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-03 16:19:25 +0100

    Implement DeleteEvent

---
+++ src/Application/Contracts/Events/DeleteEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+namespace Application.Contracts.Events;
+
+public abstract class DeleteEvent : Event
+{
+    public Dictionary<string, object> Condition { get; init; }
+
+    public DeleteEvent(IntermediateEvent intermediateEvent) : base(intermediateEvent)
+    {
+        JsonElement conditionElement = intermediateEvent.Payload.GetProperty("condition");
+        Condition = conditionElement.Deserialize<Dictionary<string, object>>();
+    }
+}
---
+++ src/Infrastructure/Events/Mappings/MySQL/MySqlDeleteEvent.cs
@@ -0,0 +1,12 @@
+﻿using Application.Contracts.Events;
+
+namespace Infrastructure.Events.Mappings.MySQL;
+
+public class MySqlDeleteEvent(IntermediateEvent intermediateEvent) : DeleteEvent(intermediateEvent)
+{
+    public override string GetCommand()
+    {
+        return
+            $"DELETE FROM {AggregateName} WHERE {Condition}";
+    }
+}
