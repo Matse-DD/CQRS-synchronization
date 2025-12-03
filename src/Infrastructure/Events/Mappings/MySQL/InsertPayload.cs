f6853778cc1317f9d48404513bfe8cb6212c15c9
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-03 14:22:26 +0100

    Implement a insert event for mysql

---
+++ src/Infrastructure/Events/Mappings/MySQL/InsertPayload.cs
@@ -0,0 +1,14 @@
+﻿
+using Application.Contracts.Events.Payloads;
+
+namespace Infrastructure.Events.Mappings.MySQL;
+
+public class InsertPayload(Dictionary<string, object> payload) : IPayload
+{
+    public Dictionary<string, object> Payload { get; init; } = payload;
+
+    public Dictionary<string, object> GetValuePairs()
+    {
+        return Payload;
+    }
+}
---
+++ src/Infrastructure/Events/Mappings/MySQL/MySqlInsertEvent.cs
@@ -0,0 +1,64 @@
+﻿using Application.Contracts.Events;
+using System.Text.Json;
+
+namespace Infrastructure.Events.Mappings.MySQL;
+
+public class MySqlInsertEvent : Event
+{
+    public MySqlInsertEvent(string incomingEvent) : base(incomingEvent)
+    {
+        Dictionary<string, object> propertiesEvent = JsonSerializer.Deserialize<Dictionary<string, object>>(incomingEvent);
+        Dictionary<string, object> insertPayload = JsonSerializer.Deserialize<Dictionary<string, object>>(propertiesEvent["payload"].ToString());
+        PayLoad = new InsertPayload(insertPayload);
+    }
+
+    public override string GetCommand()
+    {
+        return
+            $"INSERT INTO {AggregateName} ({PayLoad.GetValuePairs().Keys.ToList().Aggregate("",
+            (currentAccumulation, nextKey) =>
+            {
+                if (string.IsNullOrEmpty(currentAccumulation))
+                {
+                    return nextKey;
+                }
+                else
+                {
+                    return currentAccumulation + ", " + nextKey;
+                }
+            }
+             )})\n" +
+            $"VALUES ({getValuesReduced(PayLoad.GetValuePairs().Values)})";
+    }
+
+
+    private string getValuesReduced(IEnumerable<object> incomingValues)
+    {
+        string result = "";
+
+        foreach (JsonElement incomingValue in incomingValues)
+        {
+            if (incomingValue is JsonElement value)
+            {
+                if (value.ValueKind == JsonValueKind.String)
+                {
+                    result += $"\"{value}\"";
+                }
+                else
+                {
+                    result += incomingValue;
+                }
+
+
+                result += ", ";
+            }
+        }
+
+        if (result.Length >= 2)
+        {
+            result = result.Substring(0, result.Length - 2);
+        }
+
+        return result;
+    }
+}
