6663800dfb674e4a817f114a224344bc4a4566c6
LanderDebeir <landerdebeir@outlook.com>
2025-12-03 12:50:42 +0100

    Create mock observer

---
+++ test/ApplicationTests/Shared/MockObserver.cs
@@ -0,0 +1,15 @@
+﻿using Application.Contracts.Events;
+using Application.Contracts.Ports;
+
+namespace ApplicationTests.Shared;
+
+public class MockObserver(IEnumerable<Event>? events) : IObserver
+{
+    private List<Event> _events = events.ToList() ?? [];
+    
+    public Task StartListening(Action<Event> callback)
+    {
+        _events.ForEach(callback);
+        return Task.CompletedTask;
+    }
+}
