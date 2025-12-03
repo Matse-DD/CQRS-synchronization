1de68351cb7086fa576a751933466e5afb6fde95
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-03 13:11:57 +0100

    update using

--- test/ApplicationTests/Shared/Persistence/MockQueryRepository.cs
+++ test/ApplicationTests/Shared/Persistence/MockQueryRepository.cs
@@ -1,25 +1,28 @@
-﻿using Application.Ports.Persistence;
+﻿using Application.Contracts.Ports.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace ApplicationTests.Shared.Persistence;
 
 public class MockQueryRepository : IQueryRepository
 {
     public ICollection<string> History { get; private set; } = [];
-    private Guid _lastSuccesfulEventId;
+    private Guid lastSuccesfulEventId;
 
     public void Execute(string command, Guid eventId)
     {
         string lowerCommand = command.ToLower();
 
-        if (lowerCommand.Contains("update") || lowerCommand.Contains("delete") || lowerCommand.Contains("insert"))
+        if(lowerCommand.Contains("update") || lowerCommand.Contains("delete") || lowerCommand.Contains("insert"))
         {
             History.Add(command);
-            _lastSuccesfulEventId = eventId;
+            lastSuccesfulEventId = eventId;
         }
     }
 
     public Guid GetLastSuccessfulEventId()
     {
-        return this._lastSuccesfulEventId;
+        return this.lastSuccesfulEventId;
     }
 }
