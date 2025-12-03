ed5a56eb8df4dfdd2043c7fcdfb312663a89e545
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-03 15:21:30 +0100

    Commit before rethinking

---
+++ src/Infrastructure/Events/Mappings/MySQL/MySqlUpdateEvent.cs
@@ -0,0 +1,24 @@
+﻿using Application.Contracts.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Infrastructure.Events.Mappings.MySQL
+{
+    public class MySqlUpdateEvent : Event
+    {
+        public MySqlUpdateEvent(string incomingEvent): base(incomingEvent)
+        {
+            Dictionary<string, object> propertiesEvent = JsonSerializer.Deserialize<Dictionary<string, object>>(incomingEvent);
+            Dictionary<string, object> insertPayload = JsonSerializer.Deserialize<Dictionary<string, object>>(propertiesEvent["payload"].ToString());
+            PayLoad = new InsertPayload(insertPayload);
+        }
+        public override string GetCommand()
+        {
+            Dictionary<string, object> change = JsonSerializer.Deserialize<Dictionary<string, object>>(PayLoad.GetValuePairs()["change"]);
+
+            throw new NotImplementedException();
+        }
+    }
+}
