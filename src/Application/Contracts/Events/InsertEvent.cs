8b5967345d76dc10177ed888a6b3cc9f699018a9
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-03 16:14:10 +0100

    Create insert event

---
+++ src/Application/Contracts/Events/InsertEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+namespace Application.Contracts.Events;
+
+public abstract class InsertEvent : Event
+{
+    public Dictionary<string, object> Properties { get; init; }
+    public InsertEvent(IntermediateEvent intermediateEvent) : base(intermediateEvent)
+    {
+        Properties = intermediateEvent.Payload.Deserialize<Dictionary<string, object>>();
+    }
+}
