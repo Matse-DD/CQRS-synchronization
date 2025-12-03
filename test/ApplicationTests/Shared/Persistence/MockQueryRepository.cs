7e01d71e32f79dffc089c710c7c8696522f7e6ac
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-03 12:54:16 +0100

    Create mock query repo

--- test/ApplicationTests/Shared/Persistence/MockQueryRepository.cs
+++ test/ApplicationTests/Shared/Persistence/MockQueryRepository.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿using Application.Ports.Persistence;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ApplicationTests.Shared.Persistence
 {
-    internal class MockQueryRepository
+    public class MockQueryRepository : IQueryRepository
     {
+        public ICollection<string> History { get; private set; } = [];
+        private Guid lastSuccesfulEventId;
+
+        public void Execute(string command, Guid eventId)
+        {
+            string lowerCommand = command.ToLower();
+
+            if(lowerCommand.Contains("update") ||lowerCommand.Contains("delete") || lowerCommand.Contains("insert"))
+            {
+                History.Add(command);
+                lastSuccesfulEventId = eventId;
+            }
+
+            else
+            {
+                return;
+            }
+        }
+
+        public Guid GetLastSuccessfulEventId()
+        {
+            return this.lastSuccesfulEventId;
+        }
     }
 }
