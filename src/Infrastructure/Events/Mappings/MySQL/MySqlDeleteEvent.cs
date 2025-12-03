56779cbb87f509ac2d6401d3694b4d54e455fbc3
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-03 22:33:59 +0100

    Implement mapping delete

--- src/Infrastructure/Events/Mappings/MySQL/MySqlDeleteEvent.cs
+++ src/Infrastructure/Events/Mappings/MySQL/MySqlDeleteEvent.cs
@@ -7,6 +7,25 @@ public class MySqlDeleteEvent(IntermediateEvent intermediateEvent) : DeleteEvent
     public override string GetCommand()
     {
         return
-            $"DELETE FROM {AggregateName} WHERE {Condition}";
+            $"DELETE FROM {AggregateName} WHERE {MapWhereClause(Condition)}";
+    }
+    private string MapWhereClause(IDictionary<string, string> condition)
+    {
+        if (condition == null || !condition.Any()) return "True";
+
+        return string.Join(" AND ", condition.Select(conditionPair =>
+        {
+            string key = conditionPair.Key;
+            string value = conditionPair.Value.Trim();
+
+            if (value.StartsWith(">=") || value.StartsWith("<=") || value.StartsWith(">") || value.StartsWith("<") || value.StartsWith("="))
+            {
+                return $"{key} {value}";
+            }
+            else
+            {
+                return $"{key} = {value}";
+            }
+        }));
     }
 }
