40b266894b93d2f9f0c3d55939e700883b530886
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-03 14:45:41 +0100

    create eventfactory

---
+++ src/Application/Contracts/Events/IEventFactory.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Contracts.Events;
+
+public interface IEventFactory
+{
+    public Event DetermineEvent(string incomingEvent);
+}
---
+++ src/Infrastructure/Events/EventFactory.cs
@@ -0,0 +1,31 @@
+﻿using Application.Contracts.Events;
+using Infrastructure.Events.Mappings.MySQL;
+using System.Text.Json;
+
+namespace Infrastructure.Events;
+
+public class MySqlEventFactory : IEventFactory
+{
+
+    public Event DetermineEvent(string incomingEvent)
+    {
+        Dictionary<string, object> propertiesEvent = JsonSerializer.Deserialize<Dictionary<string, object>>(incomingEvent);
+        EventType eventType = JsonSerializer.Deserialize<EventType>(propertiesEvent["event_type"].ToString());
+
+        switch (eventType)
+        {
+            case EventType.INSERT:
+                return new MySqlInsertEvent(incomingEvent);
+
+            //case EventType.UPDATE:
+            //    return new MySqlUpdateEvent(incomingEvent);
+
+            //case EventType.DELETE:
+            //    return new MySqlDeleteEvent(incomingEvent);
+            
+        }
+
+        return null;
+    }
+}
+
