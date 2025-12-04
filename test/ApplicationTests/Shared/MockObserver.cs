d83d81d0b327e42491f3a8147ca474fdda1ef88a
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-04 14:15:43 +0100

    update observer

--- test/ApplicationTests/Shared/MockObserver.cs
+++ test/ApplicationTests/Shared/MockObserver.cs
@@ -1,13 +1,14 @@
 ﻿using Application.Contracts.Events.EventOptions;
 using Application.Contracts.Observer;
+using Application.Contracts.Persistence;
 
 namespace ApplicationTests.Shared;
 
-public class MockObserver(IEnumerable<Event>? events) : IObserver
+public class MockObserver(IEnumerable<string>? events) : IObserver
 {
-    private List<Event> _events = events?.ToList() ?? [];
+    private List<string> _events = events?.ToList() ?? [];
 
-    public Task StartListening(Action<Event> callback)
+    public Task StartListening(Action<string> callback)
     {
         _events.ForEach(callback);
         return Task.CompletedTask;
