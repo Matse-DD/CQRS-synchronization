c527ebcbbffd58e90ee085d5ae2b850fe02c695d
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-04 11:16:15 +0100

    use correct names for methods

---
+++ src/Infrastructure/Projector/Projector.cs
@@ -0,0 +1,51 @@
+﻿using Application.Contracts.Events.EventOptions;
+using Application.Contracts.Events.Factory;
+using Application.Contracts.Persistence;
+
+namespace Infrastructure.Projector;
+
+public class Projector(ICommandRepository commandRepository, IQueryRepository queryRepository, IEventFactory eventFactory)
+{
+    private readonly ICommandRepository _commandRepository = commandRepository;
+    private readonly IQueryRepository _queryRepository = queryRepository;
+    private readonly IEventFactory _eventFactory = eventFactory;
+
+    public bool Locked { private get; set; }
+
+    private ICollection<string> eventQueue = new List<string>();
+
+
+    public void AddEventsToFront(IEnumerable<string> batchOfEvents)
+    {
+        eventQueue = [.. batchOfEvents, ..eventQueue];
+
+        eventQueue = eventQueue.Distinct().ToList();
+    }
+
+    public void AddEvent(string incomingEvent)
+    {
+        eventQueue.Add(incomingEvent);
+    }
+
+    public void ProjectEvent(string eventToProject)
+    {
+        Event convertedEvent = _eventFactory.DetermineEvent(eventToProject);
+
+        string commandForEvent = convertedEvent.GetCommand();
+        Guid eventId = convertedEvent.EventId;
+
+        _queryRepository.Execute(commandForEvent, eventId);
+        _commandRepository.RemoveEvent(eventId);
+    }
+
+    public async void ProcessEvents()
+    {
+        if (Locked || ) return;
+
+
+    }
+
+
+
+
+}
