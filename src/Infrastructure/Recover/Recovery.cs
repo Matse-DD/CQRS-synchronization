167c24bc869ee484c0a5838409cbfab243524c4e
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-04 14:07:24 +0100

    Implement recovery

---
+++ src/Infrastructure/Recover/Recovery.cs
@@ -0,0 +1,23 @@
+﻿using Application.Contracts.Persistence;
+using Infrastructure.Projectors;
+
+namespace Infrastructure.Recover;
+
+public class Recovery(ICommandRepository commandRepository, IQueryRepository queryRepository, Projector projector)
+{
+    public void Recover()
+    {
+        projector.Lock();
+
+        IEnumerable<OutboxEvent> outboxEvents = commandRepository.GetAllEvents();
+        Guid lastSuccessfulEventId = queryRepository.GetLastSuccessfulEventId();
+
+        IList<string> pureEvents = [];
+        outboxEvents = outboxEvents.ToList().Where(entry => !entry.eventId.Equals(lastSuccessfulEventId));
+        outboxEvents.ToList().ForEach(entry => pureEvents.Add(entry.eventItem));
+
+        projector.AddEventsToFront(pureEvents);
+
+        projector.Unlock();
+    }
+}
