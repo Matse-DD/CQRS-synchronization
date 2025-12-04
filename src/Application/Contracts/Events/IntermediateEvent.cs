921d565fb58825c7390c09f4789509f80d74c4fa
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-04 09:46:28 +0100

    Update intermediateEvent to be able to work with the enums correctly

--- src/Application/Contracts/Events/IntermediateEvent.cs
+++ src/Application/Contracts/Events/IntermediateEvent.cs
@@ -1,33 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Text.Json;
+﻿using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace Application.Contracts.Events
+namespace Application.Contracts.Events;
+
+public class IntermediateEvent
 {
-    public class IntermediateEvent
-    {
-        [JsonPropertyName("event_id")]
-        public Guid EventId { get; init; }
+    [JsonPropertyName("event_id")]
+    public Guid EventId { get; init; }
 
-        [JsonPropertyName("occured_at")]
-        public DateTime OccuredAt { get; init; }
+    [JsonPropertyName("occured_at")]
+    public DateTime OccuredAt { get; init; }
 
-        [JsonPropertyName("aggregate_name")]
-        public string AggregateName { get; init; }
+    [JsonPropertyName("aggregate_name")]
+    public string AggregateName { get; init; }
 
-        [JsonPropertyName("status")]
-        [JsonConverter(typeof(JsonStringEnumConverter))]
-        //[JsonConverter(typeof(Status))]
-        public Status Status { get; init; }
+    [JsonPropertyName("status")]
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public Status Status { get; init; }
 
-        [JsonPropertyName("event_type")]
-        [JsonConverter(typeof(JsonStringEnumConverter))]
-        //[JsonConverter(typeof(EventType))]
-        public EventType EventType { get; init; }
+    [JsonPropertyName("event_type")]
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public EventType EventType { get; init; }
 
-        [JsonPropertyName("payload")]
-        public JsonElement Payload { get; init; }
-    }
+    [JsonPropertyName("payload")]
+    public JsonElement Payload { get; init; }
 }
