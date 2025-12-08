4bfc9d6191f7c2aff1e12d97856a5d487239ddcf
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-08 14:44:23 +0100

    update recovery

--- src/Infrastructure/Recover/Recovery.cs
+++ src/Infrastructure/Recover/Recovery.cs
@@ -5,15 +5,22 @@ namespace Infrastructure.Recover;
 
 public class Recovery(ICommandRepository commandRepository, IQueryRepository queryRepository, Projector projector)
 {
-    public async void Recover()
+    public void Recover()
     {
         projector.Lock();
 
+        StartRecovering();
+    }
+
+    public async void StartRecovering()
+    {
         IEnumerable<OutboxEvent> outboxEvents = commandRepository.GetAllEvents();
         Guid lastSuccessfulEventId = queryRepository.GetLastSuccessfulEventId();
 
         IList<string> pureEvents = [];
         outboxEvents = outboxEvents.ToList().Where(entry => !entry.eventId.Equals(lastSuccessfulEventId.ToString()));
+        // mogelijks ook kijken of event done zie mock query repo voor ideen
+
         outboxEvents.ToList().ForEach(entry => pureEvents.Add(entry.eventItem));
 
         projector.AddEventsToFront(pureEvents);
