b89e879a3d92e80efd36dc557564c284cbf4f6e8
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-08 13:53:11 +0100

    clarify needed stuff in main / program.cs

--- src/Main/Program.cs
+++ src/Main/Program.cs
@@ -1,1 +1,7 @@
 ﻿Console.WriteLine("Hello, World!");
+/*
+ * Maken van de verschillende connecties interfaces -> querydb, commanddb
+ * maken van de projector
+ * maken van de observer + starten
+ * maken van de recovery + starten
+*/
