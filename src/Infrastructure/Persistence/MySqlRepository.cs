951abdfbe12b6ad4484b17625d07d0a977c5e5c9
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-09 11:43:12 +0100

    implement execute method of mysql

--- src/Infrastructure/Persistence/MySqlRepository.cs
+++ src/Infrastructure/Persistence/MySqlRepository.cs
@@ -1,5 +1,36 @@
-﻿namespace Infrastructure.Persistence;
+﻿using Application.Contracts.Persistence;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
 
-public class MySqlRepository
+namespace Infrastructure.Persistence;
+
+public class MySqlRepository : IQueryRepository
 {
+    private readonly MySqlConnection _connection;
+
+    public MySqlRepository(string connectionString)
+    {
+        _connection = new MySqlConnection(connectionString);
+    }
+
+    public void Execute(string command, Guid eventId)
+    {
+        string commandLastEventId = $"UPDATE last_event_id SET last_event_id = {eventId} FROM last_info";
+
+        MySqlCommand cmdLastEventId = new MySqlCommand(commandLastEventId, _connection);
+        MySqlCommand cmdDataUpdate = new MySqlCommand(command, _connection);
+
+        MySqlTransaction transaction = _connection.BeginTransaction();
+
+        cmdLastEventId.ExecuteReader();
+        cmdDataUpdate.ExecuteReader();
+
+        transaction.Commit();
+    }
+
+    public Guid GetLastSuccessfulEventId()
+    {
+        throw new NotImplementedException();
+    }
 }
