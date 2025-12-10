2d975c3ff5a0d8c1131c96b38db34ae6bff53c13
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-10 13:32:13 +0100

    create integration test for recovery

--- test/IntegrationTests/ProjectorIntegration.cs
+++ test/IntegrationTests/ProjectorIntegration.cs
@@ -24,7 +24,7 @@ public class ProjectorIntegration
         MySqlConnection connectionMySql = new MySqlConnection(_connectionStringToStartRepoMySql);
         connectionMySql.Open();
 
-        string queryToStart = "CREATE DATABASE IF NOT EXISTS cqrs_read; USE cqrs_read; CREATE TABLE IF NOT EXISTS Products (product_id CHAR(36) PRIMARY KEY, name VARCHAR(255) NOT NULL,sku VARCHAR(100) NOT NULL,price DECIMAL(10,2) NOT NULL,stock_level INT NOT NULL, is_active BOOLEAN NOT NULL);CREATE TABLE IF NOT EXISTS last_info (id INT AUTO_INCREMENT PRIMARY KEY, last_event_id CHAR(36) NOT NULL);INSERT INTO last_info (last_event_id) VALUES (00000000-0000-0000-0000-000000000000);";
+        string queryToStart = "CREATE DATABASE IF NOT EXISTS cqrs_read; USE cqrs_read; CREATE TABLE IF NOT EXISTS Products (product_id CHAR(36) PRIMARY KEY, name VARCHAR(255) NOT NULL,sku VARCHAR(100) NOT NULL,price DECIMAL(10,2) NOT NULL,stock_level INT NOT NULL, is_active BOOLEAN NOT NULL);CREATE TABLE IF NOT EXISTS last_info (id INT AUTO_INCREMENT PRIMARY KEY, last_event_id CHAR(36));INSERT INTO last_info (last_event_id) VALUES (NULL);";
 
         MySqlCommand cmdGetLastEventId = new MySqlCommand(queryToStart, connectionMySql);
         await cmdGetLastEventId.ExecuteReaderAsync();
@@ -33,7 +33,7 @@ public class ProjectorIntegration
     }
 
     [Test]
-    public void Projector_Should_Project()
+    public void Recover_Should_Handle_Events_That_Are_In_Outbox()
     {
         // Set infrastructure up
         ICommandRepository commandRepo = new MongoDbCommandRepository(_connectionStringCommandRepoMongo);
@@ -61,7 +61,6 @@ public class ProjectorIntegration
         // Assert
         Task<Guid> eventId = queryRepo.GetLastSuccessfulEventId();
 
-        Console.WriteLine(eventId.Result);
         Assert.That(eventsAdded.Last().Contains((eventId.Result).ToString()), Is.EqualTo(true));
     }
 
@@ -98,7 +97,6 @@ public class ProjectorIntegration
 
         foreach(string eventItem in events)
         {
-            Console.WriteLine(eventItem);
             collection.InsertOne(BsonDocument.Parse(eventItem));
         }
 
