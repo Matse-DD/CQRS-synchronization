3802d60014b54afbbe8e9285c2e5a22b33a91cf9
Ryocery <youri.haentjens@gmail.com>
2025-12-10 16:33:34 +0100

    refactor IObserver interface by commenting out StopListening method for clarity

--- src/Application/Contracts/Observer/IObserver.cs
+++ src/Application/Contracts/Observer/IObserver.cs
@@ -3,5 +3,5 @@
 public interface IObserver
 {
     Task StartListening(Action<string> callback, CancellationToken cancellationToken);
-    void StopListening(); // Geen idee of dit nog nodig is als je met token werkt (gemakkelijker)
+    // void StopListening(); // Geen idee of dit nog nodig is als je met token werkt (gemakkelijker)
 }
