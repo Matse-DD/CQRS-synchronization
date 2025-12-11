b5325d2461094851e444b0b8f627d8700e58ec4e
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-11 14:37:15 +0100

    resolve problems with appsettings in test env

--- src/Main/Initialization/SyncBuilder.cs
+++ src/Main/Initialization/SyncBuilder.cs
@@ -23,21 +23,29 @@ public class SyncBuilder
 
     public SyncBuilder()
     {
-        string? env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        string? env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"); // TODO set ASPNETCORE_ENVIRONMENT=Production or Test from outside for production or other env
+        
+        if(env == null)
+        {
+            Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Test");
+            env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        }
+
+        Console.WriteLine(env); //TODO remove once we are sure the production env can be selected
 
         _configuration = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
-            .AddJsonFile($"appsettings.{env}.json", optional: true, reloadOnChange: false)
+            .AddJsonFile($"appsettings.{env}.json", optional: false, reloadOnChange: false)
             .Build();
     }
 
     public SyncBuilder AddRepositories()
     {
-        string connectionStringQueryRepoMySql = _configuration.GetConnectionString("ReadDatabase")
+        string connectionStringQueryRepoMySql = _configuration["ReadDatabase:ConnectionString"]
         ?? throw new InvalidOperationException("Connection string 'ReadDatabase' not found.");
 
-        string connectionStringCommandRepoMongo = _configuration.GetConnectionString("WriteDatabase")
+        string connectionStringCommandRepoMongo = _configuration["WriteDatabase:ConnectionString"]
         ?? throw new InvalidOperationException("Connection string 'WriteDatabase' not found.");
 
         _commandRepository = new MongoDbCommandRepository(connectionStringCommandRepoMongo);
@@ -65,7 +73,7 @@ public class SyncBuilder
 
     public SyncBuilder AddObserver()
     {
-        string connectionStringCommandRepoMongo = _configuration.GetConnectionString("WriteDatabase")
+        string connectionStringCommandRepoMongo = _configuration["WriteDatabase:ConnectionString"]
         ?? throw new InvalidOperationException("Connection string 'WriteDatabase' not found.");
 
         _observer = new MongoDbObserver(connectionStringCommandRepoMongo);
