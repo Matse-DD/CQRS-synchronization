083f1474385b03d6a5a20b12a36d97940a8cf8be
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-11 21:23:24 +0100

    create extension method for mongodb things

---
+++ src/Infrastructure/Tools/DatabaseExtensions/MongoDbExtensions.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Tools.DatabaseExtensions
+{
+    public static class MongoDbExtensions
+    {
+        public static BsonDocument SanitizeOccuredAt(this BsonDocument document)
+        {
+            BsonDocument clone = new BsonDocument(document);
+
+            if (clone.Contains("occurredAt") && clone["occurredAt"].IsBsonDateTime)
+            {
+                string dt = clone["occurredAt"].ToUniversalTime().ToString("o");
+                clone["occurredAt"] = new BsonString(dt);
+            }
+
+            return clone;
+        }
+    }
+}
