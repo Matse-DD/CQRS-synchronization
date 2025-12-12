475311d4933a7b3539a7848f9eb8af1c11affba9
LanderDebeir <landerdebeir@outlook.com>
2025-12-12 11:32:34 +0100

    Create replayer class

---
+++ src/Infrastructure/Replay/Replayer.cs
@@ -0,0 +1,11 @@
+﻿using Application.Contracts.Persistence;
+using Infrastructure.Projectors;
+
+namespace Infrastructure.Replay;
+
+public class Replayer(ICommandRepository commandRepository, IQueryRepository queryRepository, Projector projector)
+{
+    private readonly ICommandRepository _commandRepository = commandRepository;
+    private readonly IQueryRepository _queryRepository = queryRepository;
+    private readonly Projector projector = projector;
+}
