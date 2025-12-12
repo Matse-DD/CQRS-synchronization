cb561a7a5e522566884e36edb7996bffbdc5e181
Ryocery <youri.haentjens@gmail.com>
2025-12-12 13:54:30 +0100

    Refactor SyncBuilder to utilize dependency injection for repositories, event factory, projector, recovery, and observer

--- src/Main/Initialization/SyncBuilder.cs
+++ src/Main/Initialization/SyncBuilder.cs
@@ -8,80 +8,82 @@ using Infrastructure.Persistence.QueryRepository;
 using Infrastructure.Projectors;
 using Infrastructure.Recover;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Main.Initialization;
 
 public class SyncBuilder
 {
+    private readonly ServiceCollection _services = new();
     private readonly IConfiguration _configuration;
-    private ICommandRepository? _commandRepository;
-    private IQueryRepository? _queryRepository;
-    private IEventFactory? _eventFactory;
-    private Projector? _projector;
-    private Recovery? _recovery;
-    private IObserver? _observer;
 
     public SyncBuilder()
     {
-        string? env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"); // TODO set ASPNETCORE_ENVIRONMENT=Production or Test from outside for production or other env
-
-        if (env == null)
-        {
-            Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Test");
-            env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-        }
-
-        Console.WriteLine(env); //TODO remove once we are sure the production env can be selected
-
-        _configuration = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
+        ConfigurationBuilder configBuilder = new();
+        
+        configBuilder.SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
-            .AddJsonFile($"appsettings.{env}.json", optional: false, reloadOnChange: false)
-            .Build();
+            .AddJsonFile("appsettings.Test.json", optional: true, reloadOnChange: false)
+            .AddEnvironmentVariables();
+
+        _configuration = configBuilder.Build();
+        _services.AddSingleton(_configuration);
+
+        _services.AddLogging(builder =>
+        {
+            builder.AddConsole();
+            builder.SetMinimumLevel(LogLevel.Information);
+        });
     }
 
     public SyncBuilder AddRepositories()
     {
-        string connectionStringQueryRepoMySql = _configuration["ReadDatabase:ConnectionString"]
-        ?? throw new InvalidOperationException("Connection string 'ReadDatabase' not found.");
+        string mongoConn = _configuration["CommandDatabase:ConnectionString"] 
+        ?? throw new InvalidOperationException("Missing CommandDatabase ConnectionString");
+            
+        string mysqlConn = _configuration["QueryDatabase:ConnectionString"] 
+        ?? throw new InvalidOperationException("Missing QueryDatabase ConnectionString");
+        
+        _services.AddSingleton<ICommandRepository>(sp => new MongoDbCommandRepository(mongoConn, sp.GetRequiredService<ILogger<MongoDbCommandRepository>>()));
+        _services.AddSingleton<IQueryRepository>(sp => new MySqlQueryRepository(mysqlConn, sp.GetRequiredService<ILogger<MySqlQueryRepository>>()));
 
-        string connectionStringCommandRepoMongo = _configuration["WriteDatabase:ConnectionString"]
-        ?? throw new InvalidOperationException("Connection string 'WriteDatabase' not found.");
-
-        _commandRepository = new MongoDbCommandRepository(connectionStringCommandRepoMongo);
-        _queryRepository = new MySqlQueryRepository(connectionStringQueryRepoMySql);
         return this;
     }
 
     public SyncBuilder AddEventFactory()
     {
-        _eventFactory = new MySqlEventFactory();
+        _services.AddSingleton<IEventFactory, MySqlEventFactory>(); 
         return this;
     }
 
     public SyncBuilder AddProjector()
     {
-        _projector = new Projector(_commandRepository!, _queryRepository!, _eventFactory!);
+        _services.AddSingleton<Projector>();
         return this;
     }
 
     public SyncBuilder AddRecovery()
     {
-        _recovery = new Recovery(_commandRepository!, _queryRepository!, _projector!);
+        _services.AddSingleton<Recovery>();
         return this;
     }
 
     public SyncBuilder AddObserver()
     {
-        string connectionStringCommandRepoMongo = _configuration["WriteDatabase:ConnectionString"]
-        ?? throw new InvalidOperationException("Connection string 'WriteDatabase' not found.");
-
-        _observer = new MongoDbObserver(connectionStringCommandRepoMongo);
+        string mongoConn = _configuration["CommandDatabase:ConnectionString"] 
+        ?? throw new InvalidOperationException("Missing CommandDatabase ConnectionString");
+        
+        _services.AddSingleton<IObserver>(sp => new MongoDbObserver(mongoConn, sp.GetRequiredService<ILogger<MongoDbObserver>>()));
+            
         return this;
     }
 
     public SyncApplication Build()
     {
-        return new SyncApplication(_recovery!, _observer!, _projector!);
+        _services.AddSingleton<SyncApplication>();
+        
+        ServiceProvider provider = _services.BuildServiceProvider();
+        return provider.GetRequiredService<SyncApplication>();
     }
 }
