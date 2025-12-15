f71e0016f74eceedb311c4e1edc16acf0738fba9
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-15 22:27:35 +0100

    create basic extraction extensions for mysql

---
+++ src/Infrastructure/Events/Mappings/MySQL/ExtractionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Events.Mappings.MySQL
+{
+    public static class ExtractionExtensions
+    {
+        public static string ExtractValue(this string value)
+        {
+            if (value.Contains('\''))
+            {
+                int indexFirstQuote = value.IndexOf('\'');
+                int indexLastQuote = value.LastIndexOf('\'');
+
+                int length = indexLastQuote - indexFirstQuote - 1;
+                value = value.Substring(indexFirstQuote + 1, length);
+            }
+
+            return value;
+        }
+
+        public static string Sanitize(this string value)
+        {
+            return value.Replace("\'", "\'\'");
+        }
+
+        public static bool IsString(this string value)
+        {
+            return value.Contains('\'');
+        }
+    }
+}
