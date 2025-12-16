640bca71528eebd8d048794a4e4020139447acc5
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-16 14:18:20 +0100

    add recovery to recover before replay

--- src/Main/Initialization/SyncApplication.cs
+++ src/Main/Initialization/SyncApplication.cs
@@ -5,10 +5,11 @@ using Infrastructure.Replay;
 
 namespace Main.Initialization;
 
-public class SyncApplication(Replayer replay, IObserver observer, Projector projector)
+public class SyncApplication(Replayer replay, Recovery recover, IObserver observer, Projector projector)
 {
     public async Task RunAsync(CancellationToken cancellationToken = default)
     {
+        recover.Recover(); // TODO kijken wat we juist gaan doen met recover & replay
         replay.Replay();
         await observer.StartListening(projector.AddEvent, cancellationToken);
     }
