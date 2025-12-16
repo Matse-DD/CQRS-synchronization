e89f816a4e3858adec5a219b8d54f6a3ff5f2f82
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-16 19:16:41 +0100

    use scoped namespace

--- src/Infrastructure/Events/Mappings/MySQL/Shared/SharedMySqlMappings.cs
+++ src/Infrastructure/Events/Mappings/MySQL/Shared/SharedMySqlMappings.cs
@@ -1,27 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace Infrastructure.Events.Mappings.MySQL.Shared;
 
-namespace Infrastructure.Events.Mappings.MySQL.Shared
+public static class SharedMySqlMappings
 {
-    public static class SharedMySqlMappings
+    public static string MapWhereClause(IDictionary<string, string>? condition)
     {
-        public static string MapWhereClause(IDictionary<string, string>? condition)
+        if (condition == null || !condition.Any()) return "True";
+
+        return string.Join(" AND ", condition.Select(conditionPair =>
         {
-            if (condition == null || !condition.Any()) return "True";
+            string key = conditionPair.Key;
+            string value = conditionPair.Value.Trim();
 
-            return string.Join(" AND ", condition.Select(conditionPair =>
+            if (value.StartsWith(">=") || value.StartsWith("<=") || value.StartsWith('>') || value.StartsWith('<') || value.StartsWith('='))
             {
-                string key = conditionPair.Key;
-                string value = conditionPair.Value.Trim();
+                return $"{key}{value.DetermineMySqlValue()}";
+            }
 
-                if (value.StartsWith(">=") || value.StartsWith("<=") || value.StartsWith('>') || value.StartsWith('<') || value.StartsWith('='))
-                {
-                    return $"{key}{value.DetermineMySqlValue()}";
-                }
-
-                return $"{key} = {value.DetermineMySqlValue()}";
-            }));
-        }
+            return $"{key} = {value.DetermineMySqlValue()}";
+        }));
     }
 }
