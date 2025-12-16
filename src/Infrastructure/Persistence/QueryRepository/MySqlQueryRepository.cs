cb8b6e29cdd7a05572ae7829c0b35fc7c274cdbd
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-16 16:03:33 +0100

    add semicolons

--- src/Infrastructure/Persistence/QueryRepository/MySqlQueryRepository.cs
+++ src/Infrastructure/Persistence/QueryRepository/MySqlQueryRepository.cs
@@ -101,9 +101,9 @@ public class MySqlQueryRepository(string connectionString, ILogger<MySqlQueryRep
     public async static Task CreateBasicStructureQueryDatabase(string queryDatabaseName, string connectionString, ILogger<MySqlQueryRepository> logger)
     {
         string commandCreateBasicStructure =
-                                    $"CREATE DATABASE {queryDatabaseName}" +
-                                    $"CREATE TABLE IF NOT EXISTS last_info (id INT, last_event_id VARCHAR(36), PRIMARY(id))" +
-                                    $"INSERT INTO last_info VALUES(1, '{Guid.Empty}')";
+                                    $"CREATE DATABASE {queryDatabaseName};" +
+                                    $"CREATE TABLE IF NOT EXISTS last_info (id INT, last_event_id VARCHAR(36), PRIMARY(id));" +
+                                    $"INSERT INTO last_info VALUES(1, '{Guid.Empty}');";
 
         using MySqlConnection connection = new MySqlConnection(connectionString);
         await connection.OpenAsync();
