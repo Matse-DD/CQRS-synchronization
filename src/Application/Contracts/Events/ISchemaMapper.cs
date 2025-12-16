deb5a59b2453a7110d046176aeecdc07efe14be7
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-16 13:38:34 +0100

    create interface for the schemamapper

---
+++ src/Application/Contracts/Events/ISchemaMapper.cs
@@ -0,0 +1,8 @@
+﻿using Application.Contracts.Events.EventOptions;
+using Application.Contracts.Persistence;
+namespace Application.Contracts.Events;
+
+public interface ISchemaMapper
+{
+    public Task Map(IQueryRepository queryRepository, InsertEvent insertEvent);
+}
