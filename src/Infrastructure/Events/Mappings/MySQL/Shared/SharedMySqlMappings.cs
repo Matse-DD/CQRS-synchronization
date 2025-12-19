b7e3c71db0edb09add6eb440566c411461b81bf6
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-19 08:46:44 +0100

    create a few methods for the checks in mapwhereclause

--- src/Infrastructure/Events/Mappings/MySQL/Shared/SharedMySqlMappings.cs
+++ src/Infrastructure/Events/Mappings/MySQL/Shared/SharedMySqlMappings.cs
@@ -1,22 +1,35 @@
-﻿namespace Infrastructure.Events.Mappings.MySQL.Shared;
+﻿using Google.Protobuf.WellKnownTypes;
+using static Mysqlx.Expect.Open.Types.Condition.Types;
+
+namespace Infrastructure.Events.Mappings.MySQL.Shared;
 
 public static class SharedMySqlMappings
 {
     public static string MapWhereClause(IDictionary<string, string>? condition)
     {
-        if (condition == null || !condition.Any()) return "True";
+        if (!DoesConditionExist(condition)) return "True";
 
-        return string.Join(" AND ", condition.Select(conditionPair =>
+        return string.Join(" AND ", condition!.Select(conditionPair =>
         {
             string key = conditionPair.Key;
             string value = conditionPair.Value.Trim();
 
-            if (value.StartsWith(">=") || value.StartsWith("<=") || value.StartsWith('>') || value.StartsWith('<') || value.StartsWith('='))
+            if (HasConditionSign(value))
             {
                 return $"{key}{value.DetermineMySqlValue()}";
             }
 
             return $"{key} = {value.DetermineMySqlValue()}";
         }));
+    }
+
+    private static bool HasConditionSign(string value)
+    {
+        return value.StartsWith(">=") || value.StartsWith("<=") || value.StartsWith('>') || value.StartsWith('<') || value.StartsWith('=');
+    }
+
+    private static bool DoesConditionExist(IDictionary<string, string>? condition)
+    {
+        return condition == null || !condition.Any();
     }
 }
