b51b4149399dc7fb496e1b072983b607a657d5ba
De Deyn Matse <matse.de.deyn@student.howest.be>
2025-12-31 16:48:00 +0100

    make the swagger site start up routes are still not seen

--- src/Main/Program.cs
+++ src/Main/Program.cs
@@ -1,4 +1,8 @@
-﻿using Main.Initialization;
+﻿using Infrastructure.WebApi;
+using Main.Initialization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 using ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
@@ -15,4 +19,53 @@ SyncApplication app = await new SyncBuilder()
     .AddObserver()
     .Build();
 
-await app.RunAsync(CancellationToken.None);
+// TODOD kijken om de webappbuilder te gebruiken een webapp te maken en deze toe tevoegen
+// // of te starten in de build/services
+WebApplicationBuilder webAppBuilder = WebApplication.CreateBuilder();
+
+webAppBuilder.Services.AddEndpointsApiExplorer();
+
+webAppBuilder.Services
+    .AddOpenApi(options =>
+    {
+        options.AddDocumentTransformer((document, context, _) =>
+        {
+            document.Info = Routes.OpenApiInfo;
+            return Task.CompletedTask;
+        });
+    })
+    .AddProblemDetails()
+        .AddCors(options =>
+        {
+            options.AddDefaultPolicy(builder =>
+            {
+                builder
+                    .WithExposedHeaders("*")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowAnyOrigin();
+            });
+        });
+
+webAppBuilder.Services.AddScoped<SyncApplication>();
+
+WebApplication webApp = webAppBuilder.Build();
+
+webApp.UseCors();
+
+webApp.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/openapi/v1.json", "v1");
+});
+
+webApp.UseHttpsRedirection();
+
+webApp.MapRoutes();
+webApp.MapOpenApi();
+
+
+Task runningSyncApp = app.RunAsync(CancellationToken.None);
+
+Task runningWebApp = webApp.RunAsync();
+
+await Task.WhenAll(runningSyncApp, runningWebApp);
