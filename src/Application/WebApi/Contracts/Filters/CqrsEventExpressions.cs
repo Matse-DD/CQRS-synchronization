33a23a92db13e44fb80f845287d4c99f8c9b9092
De Deyn Matse <matse.de.deyn@student.howest.be>
2026-01-02 11:24:02 +0100

    create contracts for webapi

---
+++ src/Application/WebApi/Contracts/Filters/CqrsEventExpressions.cs
@@ -0,0 +1,12 @@
+﻿using Application.Contracts.Events.EventOptions;
+using System.Linq.Expressions;
+
+namespace Application.WebApi.Contracts.Filters;
+
+public static class CqrsEventExpressions
+{
+    public static Expression<Func<Event, bool>> CqrsEventsBasedOnStatusAndBeforeTime(string status, DateTime beforeTime)
+    {
+        return cqrsEvent => cqrsEvent.Status.Equals(status) && cqrsEvent.OccuredAt <= beforeTime; 
+    }
+}
---
+++ src/Application/WebApi/Contracts/Ports/Queries.cs
@@ -0,0 +1,10 @@
+﻿using Application.Contracts.Events.EventOptions;
+using System.Linq.Expressions;
+
+namespace Application.WebApi.Contracts.Ports
+{
+    public interface IGetEventsByFiltersQuery
+    {
+        Task<IReadOnlyList<Event>> Fetch(Expression<Func<Event, bool>> filter);
+    }
+}
