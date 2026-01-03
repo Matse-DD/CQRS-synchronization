c3b6516b0046d414ad7eca8e46c178d5d7e81ca1
De Deyn Matse <matse.de.deyn@student.howest.be>
2026-01-03 17:18:57 +0100

    Create contracts to get events based on a filter

--- src/Application/WebApi/Contracts/Filters/CqrsEventExpressions.cs
+++ src/Application/WebApi/Contracts/Filters/CqrsEventExpressions.cs
@@ -5,8 +5,12 @@ namespace Application.WebApi.Contracts.Filters;
 
 public static class CqrsEventExpressions
 {
-    public static Expression<Func<Event, bool>> CqrsEventsBasedOnStatusAndBeforeTime(string status, DateTime beforeTime)
+    public static Expression<Func<Event, bool>> CqrsEventsBasedOnStatusAndBeforeTime(string? status, DateTime? beforeTime)
     {
+        if (status == null && beforeTime == null) return cqrsEvent => true;
+        if (status == null) return cqrsEvent => cqrsEvent.OccuredAt <= beforeTime;
+        if (beforeTime == null) return cqrsEvent => cqrsEvent.Status.Equals(status);
+
         return cqrsEvent => cqrsEvent.Status.Equals(status) && cqrsEvent.OccuredAt <= beforeTime; 
     }
 }
--- src/Application/WebApi/CqrsEvents/GetEventsByFilters.cs
+++ src/Application/WebApi/CqrsEvents/GetEventsByFilters.cs
@@ -1,18 +1,19 @@
 ﻿using Application.Contracts.Events.EventOptions;
+using Application.Contracts.Persistence;
 using Application.WebApi.Contracts.Filters;
 using Application.WebApi.Contracts.Ports;
 
 namespace Application.WebApi.Events;
 
-public sealed record GetEventsByFilterInput(string Status, DateTime BeforeTime);
+public sealed record GetEventsByFiltersInput(string? Status, DateTime? BeforeTime);
 
 public sealed class GetEventsByFilters(
     IGetEventsByFiltersQuery getEventsByFiltersQuery
-) : IUseCase<GetEventsByFilterInput, Task<IReadOnlyList<Event>>>
+) : IUseCase<GetEventsByFiltersInput, Task<IReadOnlyList<Event>>>
 {
     private readonly IGetEventsByFiltersQuery _getEventsByFiltersQuery = getEventsByFiltersQuery;
 
-    public async Task<IReadOnlyList<Event>> Execute(GetEventsByFilterInput input)
+    public async Task<IReadOnlyList<Event>> Execute(GetEventsByFiltersInput input)
     {
         IReadOnlyList<Event> cqrsEvents = await _getEventsByFiltersQuery.Fetch(CqrsEventExpressions.CqrsEventsBasedOnStatusAndBeforeTime(input.Status, input.BeforeTime));
 
