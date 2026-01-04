bcf14418533f2ded661c5f7a70831d7dff7f9726
De Deyn Matse <matse.de.deyn@student.howest.be>
2026-01-04 19:43:10 +0100

    Clean program.cs further up

--- src/Main/Program.cs
+++ src/Main/Program.cs
@@ -17,37 +17,27 @@ SyncBuilder syncBuilder = new SyncBuilder()
     .AddProjector()
     .AddRecovery()
     .AddReplay()
-    .AddObserver(); //TODO check if there is noway to just add the syncapplication
+    .AddObserver(); 
 
-// TODOD kijken om de webappbuilder te gebruiken een webapp te maken en deze toe tevoegen
-// // of te starten in de build/services
 WebApplicationBuilder webAppBuilder = WebApplication.CreateBuilder(args);
 
-foreach (var service in syncBuilder.GetServices()) // TODO look for a better solution 
-{
-    webAppBuilder.Services.Add(service);
-}
-
+webAppBuilder.Services.AddSyncInfrastructure(syncBuilder);
 webAppBuilder.Services.AddQueries();
 webAppBuilder.Services.AddUseCases();
-
 webAppBuilder.Services.AddWebApiModules();
 
-webAppBuilder.Services.AddScoped<SyncApplication>();
-
 WebApplication webApp = webAppBuilder.Build();
+
 webApp.UseWebApiModules();
 
-await MySqlQueryRepository.CreateBasicStructureQueryDatabase(
-    syncBuilder.QueryDatabaseName(),
-    syncBuilder.ConnectionString(),
+await AdditionalServices.SetBasicDatabaseStructureUp(
+    syncBuilder, 
     webApp.Services.GetRequiredService<ILogger<MySqlQueryRepository>>()
 );
 
 SyncApplication app = webApp.Services.GetRequiredService<SyncApplication>();
 
 Task runningSyncApp = app.RunAsync(CancellationToken.None);
-
 Task runningWebApp = webApp.RunAsync();
 
 await Task.WhenAll(runningSyncApp, runningWebApp);
---
+++ src/Main/WebApi/AdditionalServices.cs
@@ -0,0 +1,31 @@
+﻿using Infrastructure.Persistence.QueryRepository;
+using Main.Initialization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Runtime.CompilerServices;
+
+namespace Main.WebApi;
+
+public static class AdditionalServices
+{
+    public static IServiceCollection AddSyncInfrastructure(this IServiceCollection services, SyncBuilder syncBuilder)
+    {
+        foreach (var service in syncBuilder.GetServices())
+        {
+            services.Add(service);
+        }
+
+        services.AddScoped<SyncApplication>();
+        return services;
+    }
+
+    public static async Task SetBasicDatabaseStructureUp(SyncBuilder syncBuilder, ILogger<MySqlQueryRepository> mySqlQueryLogger)
+    {
+        await MySqlQueryRepository.CreateBasicStructureQueryDatabase(
+            syncBuilder.QueryDatabaseName(),
+            syncBuilder.ConnectionString(),
+            mySqlQueryLogger
+        );
+    }
+}
