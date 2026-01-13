a9d94167cf36933465a34315923f603fbb93be20
Lohani Pratik <pratik.lohani@student.howest.be>
2026-01-13 14:50:18 +0100

    feat: implement setup method for MongoDbCommandRepository integration tests

--- test/IntegrationTests/Persistence/TestMongoDbEventOperations.cs
+++ test/IntegrationTests/Persistence/TestMongoDbEventOperations.cs
@@ -14,4 +14,13 @@ public class TestMongoDbEventOperations
 
     [SetUp]
     public async Task SetUp()
-    {}}
+    {
+        MongoUrl url = new(ConnectionStringCommandRepoMongo);
+        MongoClient client = new MongoClient(url);
+        IMongoDatabase? database = client.GetDatabase(url.DatabaseName);
+        _collection = database.GetCollection<BsonDocument>("events");
+
+        await _collection.DeleteManyAsync(Builders<BsonDocument>.Filter.Empty);
+
+        _repository = new MongoDbCommandRepository(ConnectionStringCommandRepoMongo, NullLogger<MongoDbCommandRepository>.Instance);
+    }}
