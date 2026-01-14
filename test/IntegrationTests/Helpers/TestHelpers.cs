690e1f18fa4cd6cf4df76852de634006c4e470eb
Lohani Pratik <pratik.lohani@student.howest.be>
2026-01-14 13:35:14 +0100

    Add TestHelpers class with AssertEventuallyAsync method for asynchronous condition assertions

---
+++ test/IntegrationTests/Helpers/TestHelpers.cs
@@ -0,0 +1,20 @@
+namespace IntegrationTests.Helpers;
+
+public static class TestHelpers
+{
+
+    public static async Task AssertEventuallyAsync(Func<Task<bool>> condition, int timeoutMs = 5000, int intervalMs = 100)
+    {
+        DateTime start = DateTime.UtcNow;
+        while ((DateTime.UtcNow - start).TotalMilliseconds < timeoutMs)
+        {
+            if (await condition())
+            {
+                return;
+            }
+            await Task.Delay(intervalMs);
+        }
+
+        throw new TimeoutException($"Condition was not met within {timeoutMs}ms");
+    }
+}
