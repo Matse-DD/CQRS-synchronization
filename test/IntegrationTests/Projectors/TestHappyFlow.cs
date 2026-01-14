cd91003f08950f83119cf4a0e1180682c290ca45
Lohani Pratik <pratik.lohani@student.howest.be>
2026-01-14 16:52:06 +0100

    Implement setup method for TestHappyFlow to clean database and initialize repositories

--- test/IntegrationTests/Projectors/TestHappyFlow.cs
+++ test/IntegrationTests/Projectors/TestHappyFlow.cs
@@ -37,4 +37,33 @@ public class TestHappyFlow
         await using MySqlCommand cmdGetLastEventId = new MySqlCommand(queryToStart, connectionMySql);
         await cmdGetLastEventId.ExecuteNonQueryAsync();
     }
+
+    [SetUp]
+    public async Task SetUp()
+    {
+        await using MySqlConnection connectionMySql = new MySqlConnection(ConnectionStringQueryRepoMySql);
+        await connectionMySql.OpenAsync();
+        const string cleanupSql = "DROP TABLE IF EXISTS Products; DELETE FROM last_info WHERE collection_name = 'events'";
+        await using MySqlCommand cmd = new MySqlCommand(cleanupSql, connectionMySql);
+        await cmd.ExecuteNonQueryAsync();
+
+        MongoUrl mongoUrl = new(ConnectionStringCommandRepoMongo);
+        MongoClient client = new MongoClient(mongoUrl);
+        IMongoDatabase? database = client.GetDatabase(mongoUrl.DatabaseName);
+        IMongoCollection<BsonDocument>? collection = database.GetCollection<BsonDocument>("events");
+        await collection.DeleteManyAsync(Builders<BsonDocument>.Filter.Empty);
+
+        _commandRepo = new MongoDbCommandRepository(ConnectionStringCommandRepoMongo, NullLogger<MongoDbCommandRepository>.Instance);
+        _queryRepo = new MySqlQueryRepository(ConnectionStringQueryRepoMySql, NullLogger<MySqlQueryRepository>.Instance);
+        IEventFactory eventFactory = new MySqlEventFactory();
+        ISchemaBuilder schemaBuilder = new MySqlSchemaBuilder();
+
+        _projector = new Projector(_commandRepo, _queryRepo, eventFactory, NullLogger<Projector>.Instance, schemaBuilder);
+        _observer = new MongoDbObserver(ConnectionStringCommandRepoMongo, NullLogger<MongoDbObserver>.Instance);
+
+        _cancellationTokenSource = new CancellationTokenSource();
+        _ = _observer.StartListening(_projector.AddEvent, _cancellationTokenSource.Token);
+        
+        await Task.Delay(500);
+    }
 }
