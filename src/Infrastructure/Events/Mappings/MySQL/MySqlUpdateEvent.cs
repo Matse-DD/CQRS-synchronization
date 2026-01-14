959c0e2790c2e58237be0197b932f6bda5a35d02
De Deyn Matse <matse.de.deyn@student.howest.be>
2026-01-14 12:21:35 +0100

    Update update so it can be safe

--- src/Infrastructure/Events/Mappings/MySQL/MySqlUpdateEvent.cs
+++ src/Infrastructure/Events/Mappings/MySQL/MySqlUpdateEvent.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Contracts.Events.EventOptions;
 using Application.Contracts.Events.Factory;
 using Infrastructure.Events.Mappings.MySQL.Shared;
+using Infrastructure.Events.Mappings.Shared;
 using Infrastructure.Persistence;
 
 namespace Infrastructure.Events.Mappings.MySQL;
@@ -9,19 +10,32 @@ public class MySqlUpdateEvent(IntermediateEvent intermediateEvent) : UpdateEvent
 {
     public override PersistenceCommandInfo GetCommandInfo()
     {
-        string command = $"UPDATE {AggregateName}\n" +
-                         $"SET {MapSet(Change)}\n" +
-                         $"WHERE {SharedMySqlMappings.MapWhere(Condition)}";
+        string command = $"UPDATE {AggregateName.Sanitize()}\n" +
+                         $"SET {MapSet(nameof(Change), Change)}\n" +
+                         $"WHERE {SharedMySqlMappings.MapWhere(nameof(Condition),Condition)}";
 
-        return new PersistenceCommandInfo(command);
+        Dictionary<string, object> parametersCondition = SharedMySqlMappings.MapValuesToParameters(nameof(Condition), Condition);
+        Dictionary<string, object> parametersChange = SharedMySqlMappings.MapValuesToParameters(nameof(Change), Change);
+
+        Dictionary<string, object> combined = parametersCondition.Concat(parametersChange)
+                                                                 .ToDictionary(k => k.Key, v => v.Value);
+        return new PersistenceCommandInfo(command, combined);
     }
 
-    private static string MapSet(IDictionary<string, string> change)
+    private static string MapSet(string prefix, IDictionary<string, string> change)
     {
-        return string.Join(", ", change.Select(changePair =>
-            {
-                return $"{changePair.Key} = {changePair.Value.DetermineMySqlValue()}";
-            }
-        ));
+        return string.Join(", ", change.Select(changePair => ParameterizedChange(prefix, changePair)));
+    }
+
+    private static string ParameterizedChange(string prefix, KeyValuePair<string, string> changePart)
+    {
+        return $"{changePart.Key} = {DetermineChange(prefix, changePart.Key, changePart.Value)}";
+    }
+
+    private static string DetermineChange(string prefix, string key, string change)
+    {
+        string sign = change.ExtractSign();
+
+        return $"{key} {sign} @{prefix}_{key}";
     }
 }
